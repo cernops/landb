a365b39ec0cb05437ab11771c2783b9d8bbbc722
Joe Harrison <joe.harrison@cern.ch>
2013-12-03 12:49:02 +0100

    Added setuptools setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    """Open a filename and return the contents as a string.
+
+    :param fname: Filename (relative to this file) to read
+    :type fname: str
+
+    :returns: str -- file contents
+
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='LANDB',
+    version='0.1',
+    url='https://github.com/cernops/landb',
+    license='GPLv3',
+    author='Joe Harrison',
+    author_email='joe.harrison@cern.ch',
+    description='A Python library designed to interface with CERN\'s LANDB '
+    'network information system',
+    long_description=read('README.md'),
+    packages=['landb'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'suds>=0.4'
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Database :: Front-Ends'
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
